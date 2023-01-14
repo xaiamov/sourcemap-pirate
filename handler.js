3c5aadd04682f94cedbf79790a52c1a5c86f9d5f
xaiamov <63728400+xaiamov@users.noreply.github.com>
2023-01-14 08:50:45 +0200

    Now works from command line with local file system
    Usage: node handler.js <path to js.map>
    Writes output JS source into /tmp/generated folder

--- handler.js
+++ handler.js
@@ -1,18 +1,19 @@
 const sourceMap = require("source-map");
 const fs = require("fs");
 const mkdirp = require("mkdirp");
-const getDirName = require("path").dirname;
-const fetch = require("node-fetch");
-const { zip } = require("zip-a-folder");
+const { dirname } = require("path");
 
-const getRawSourceMap = url =>
-  fetch(url)
-    .then(res => res.json())
-    .then(json => json);
+function getRawSourceMap(file) {
+	try {
+		const data = fs.readFileSync(file, 'utf8');
+		return JSON.parse(data);
+	} catch (err) {
+		console.error(err);
+	}
+}
 
 async function writeFolderAndFile(path, content) {
-  await mkdirp(getDirName("/tmp/generated/" + path));
-
+  await mkdirp(dirname("/tmp/generated/" + path));
   fs.writeFileSync("/tmp/generated/" + path, content);
 }
 
@@ -25,17 +26,6 @@ async function writeSourceToFile(consumer) {
   );
 }
 
-async function generateSource(url) {
-  console.log("generateSource");
-
-  const parsedMap = await getRawSourceMap(url);
-  await sourceMap.SourceMapConsumer.with(
-    parsedMap,
-    null,
-    writeSourceToFile
-  ).catch(error => handleError(error));
-}
-
 function handleError(message) {
   console.log(message);
   return {
@@ -44,28 +34,14 @@ function handleError(message) {
   };
 }
 
-module.exports.generate = async event => {
-  console.log(event);
+async function main() {
+  var file = process.argv[2];
+	const parsedMap = await getRawSourceMap(file);
+	await sourceMap.SourceMapConsumer.with(
+		parsedMap,
+		null,
+		writeSourceToFile
+	).catch(error => handleError(error));
+}
 
-  if (event && event.queryStringParameters && event.queryStringParameters.url) {
-    const url = event.queryStringParameters.url;
-
-    await generateSource(url)
-      .then(async () => {
-        await zip("/tmp/generated", "/tmp/archive.zip");
-      })
-      .catch(e => handleError(e));
-
-    return {
-      headers: {
-        "Content-Type": "application/zip, application/octet-stream",
-        "Content-disposition": "attachment; filename=archive.zip"
-      },
-      body: fs.readFileSync("/tmp/archive.zip").toString("base64"),
-      isBase64Encoded: true,
-      statusCode: 200
-    };
-  } else {
-    return handleError("A url is required");
-  }
-};
+main();
